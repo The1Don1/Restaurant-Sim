67b565e9a0eb4ecff92ff5c897a29c060fa6db8e
mathenda <mathendam@gmail.com>
2023-10-31 19:49:01 +0200

    Initial setup, aadding Cmake rules to repo

---
+++ Code/Project/CMakeFiles/Project.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/mathe/Documents/School/cos214/Project/AbstractTable.cpp" "CMakeFiles/Project.dir/AbstractTable.cpp.o" "gcc" "CMakeFiles/Project.dir/AbstractTable.cpp.o.d"
+  "/mnt/c/Users/mathe/Documents/School/cos214/Project/App.cpp" "CMakeFiles/Project.dir/App.cpp.o" "gcc" "CMakeFiles/Project.dir/App.cpp.o.d"
+  "/mnt/c/Users/mathe/Documents/School/cos214/Project/Customer.cpp" "CMakeFiles/Project.dir/Customer.cpp.o" "gcc" "CMakeFiles/Project.dir/Customer.cpp.o.d"
+  "/mnt/c/Users/mathe/Documents/School/cos214/Project/Engine.cpp" "CMakeFiles/Project.dir/Engine.cpp.o" "gcc" "CMakeFiles/Project.dir/Engine.cpp.o.d"
+  "/mnt/c/Users/mathe/Documents/School/cos214/Project/Facade.cpp" "CMakeFiles/Project.dir/Facade.cpp.o" "gcc" "CMakeFiles/Project.dir/Facade.cpp.o.d"
+  "/mnt/c/Users/mathe/Documents/School/cos214/Project/Floor.cpp" "CMakeFiles/Project.dir/Floor.cpp.o" "gcc" "CMakeFiles/Project.dir/Floor.cpp.o.d"
+  "/mnt/c/Users/mathe/Documents/School/cos214/Project/Mediator.cpp" "CMakeFiles/Project.dir/Mediator.cpp.o" "gcc" "CMakeFiles/Project.dir/Mediator.cpp.o.d"
+  "/mnt/c/Users/mathe/Documents/School/cos214/Project/Table.cpp" "CMakeFiles/Project.dir/Table.cpp.o" "gcc" "CMakeFiles/Project.dir/Table.cpp.o.d"
+  "/mnt/c/Users/mathe/Documents/School/cos214/Project/Waiter.cpp" "CMakeFiles/Project.dir/Waiter.cpp.o" "gcc" "CMakeFiles/Project.dir/Waiter.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Code/Project/CMakeFiles/Project.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "/mnt/c/Users/mathe/Documents/School/cos214/Project"
+  "CMakeFiles/Project.dir/AbstractTable.cpp.o"
+  "CMakeFiles/Project.dir/AbstractTable.cpp.o.d"
+  "CMakeFiles/Project.dir/App.cpp.o"
+  "CMakeFiles/Project.dir/App.cpp.o.d"
+  "CMakeFiles/Project.dir/Customer.cpp.o"
+  "CMakeFiles/Project.dir/Customer.cpp.o.d"
+  "CMakeFiles/Project.dir/Engine.cpp.o"
+  "CMakeFiles/Project.dir/Engine.cpp.o.d"
+  "CMakeFiles/Project.dir/Facade.cpp.o"
+  "CMakeFiles/Project.dir/Facade.cpp.o.d"
+  "CMakeFiles/Project.dir/Floor.cpp.o"
+  "CMakeFiles/Project.dir/Floor.cpp.o.d"
+  "CMakeFiles/Project.dir/Mediator.cpp.o"
+  "CMakeFiles/Project.dir/Mediator.cpp.o.d"
+  "CMakeFiles/Project.dir/Table.cpp.o"
+  "CMakeFiles/Project.dir/Table.cpp.o.d"
+  "CMakeFiles/Project.dir/Waiter.cpp.o"
+  "CMakeFiles/Project.dir/Waiter.cpp.o.d"
+  "Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
