d6faed3e1acf3991fa729dad860740d942ec20d7
The1Don1 <mathendam@gmail.com>
2023-11-02 13:34:18 +0200

    Squashed commit of the following:
    commit a7b7a753da538a6cd2b469a55fdb73aa7b67a82f
    Author: mathenda <mathendam@gmail.com>
    Date:   Thu Nov 2 13:19:58 2023 +0200
    
        build - added new CmakeList rules
    
        Signed-off-by: Mathenda <mathendam@gmail.com>
    
    commit e35125d22697118ae1a94c46071dbc8f4bc8928c
    Author: mathenda <mathendam@gmail.com>
    Date:   Wed Nov 1 09:22:42 2023 +0200
    
        Initial setup, added project src files
    
        Signed-off-by: mathenda <mathendam@gmail.com>
    
    commit 325ac83a59303623e40a1628a1f8d406d9fce49c
    Author: mathenda <mathendam@gmail.com>
    Date:   Tue Oct 31 20:05:28 2023 +0200
    
        Initial setup, testing Branching rules by adding new file to newly added Dir
    
        Signed-off-by: mathenda <mathendam@gmail.com>
    
    commit fb4e6960680c360d9f650ec4b178a2d968c2be96
    Author: mathenda <mathendam@gmail.com>
    Date:   Tue Oct 31 20:04:35 2023 +0200
    
        Initial setup, testing Branching rules by adding new file to newly added Dir

--- Code/Project/Project/cmake-build-debug/cmake_install.cmake
+++ Code/Project/Project/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/mathe/Documents/School/cos214/Project
+# Install script for directory: C:/Users/mathe/Documents/School/cos214/Restaurant-Sim/Code/Project/Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/mathe/Documents/School/cos214/Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/mathe/Documents/School/cos214/Restaurant-Sim/Code/Project/Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
