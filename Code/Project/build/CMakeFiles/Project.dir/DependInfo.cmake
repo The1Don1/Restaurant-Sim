e3949e45dcadcbd1d6e4e8951dcb02a9db216392
The1Don1 <mathendam@gmail.com>
2023-11-02 22:05:46 +0200

    -test remade makefile

---
+++ Code/Project/build/CMakeFiles/Project.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/App.cpp" "CMakeFiles/Project.dir/App.cpp.o" "gcc" "CMakeFiles/Project.dir/App.cpp.o.d"
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/Bill.cpp" "CMakeFiles/Project.dir/Bill.cpp.o" "gcc" "CMakeFiles/Project.dir/Bill.cpp.o.d"
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/Chef.cpp" "CMakeFiles/Project.dir/Chef.cpp.o" "gcc" "CMakeFiles/Project.dir/Chef.cpp.o.d"
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/Customer.cpp" "CMakeFiles/Project.dir/Customer.cpp.o" "gcc" "CMakeFiles/Project.dir/Customer.cpp.o.d"
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/Dish.cpp" "CMakeFiles/Project.dir/Dish.cpp.o" "gcc" "CMakeFiles/Project.dir/Dish.cpp.o.d"
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/Engine.cpp" "CMakeFiles/Project.dir/Engine.cpp.o" "gcc" "CMakeFiles/Project.dir/Engine.cpp.o.d"
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/Facade.cpp" "CMakeFiles/Project.dir/Facade.cpp.o" "gcc" "CMakeFiles/Project.dir/Facade.cpp.o.d"
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/Floor.cpp" "CMakeFiles/Project.dir/Floor.cpp.o" "gcc" "CMakeFiles/Project.dir/Floor.cpp.o.d"
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/Kitchen.cpp" "CMakeFiles/Project.dir/Kitchen.cpp.o" "gcc" "CMakeFiles/Project.dir/Kitchen.cpp.o.d"
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/Manager.cpp" "CMakeFiles/Project.dir/Manager.cpp.o" "gcc" "CMakeFiles/Project.dir/Manager.cpp.o.d"
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/Menu.cpp" "CMakeFiles/Project.dir/Menu.cpp.o" "gcc" "CMakeFiles/Project.dir/Menu.cpp.o.d"
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/Tab.cpp" "CMakeFiles/Project.dir/Tab.cpp.o" "gcc" "CMakeFiles/Project.dir/Tab.cpp.o.d"
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/Table.cpp" "CMakeFiles/Project.dir/Table.cpp.o" "gcc" "CMakeFiles/Project.dir/Table.cpp.o.d"
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/TableIterator.cpp" "CMakeFiles/Project.dir/TableIterator.cpp.o" "gcc" "CMakeFiles/Project.dir/TableIterator.cpp.o.d"
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/Visitor.cpp" "CMakeFiles/Project.dir/Visitor.cpp.o" "gcc" "CMakeFiles/Project.dir/Visitor.cpp.o.d"
+  "/mnt/c/Users/mathe/OneDrive/Documents/GitHub/Restaurant-Sim/Code/Project/Project/Waiter.cpp" "CMakeFiles/Project.dir/Waiter.cpp.o" "gcc" "CMakeFiles/Project.dir/Waiter.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
