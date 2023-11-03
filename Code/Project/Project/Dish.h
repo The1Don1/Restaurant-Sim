68eaaa6df41fef7ec561f1bade9db7f5cb5693f4
The1Don1 <84033940+The1Don1@users.noreply.github.com>
2023-11-03 22:02:13 +0200

    Sage (#13)
    * chore- docs- ci- added makefile testing to all branches makefile.yml (#9)
    
    * On branch Sage
    Changes to be committed:
    	modified:   Code/Project/Project/Chef.cpp
    	modified:   Code/Project/Project/Chef.h
    	modified:   Code/Project/Project/Dish.cpp
    	modified:   Code/Project/Project/Dish.h
    
    * Add files via upload
    
    * On branch Sage
    Changes to be committed:
    	feat - implemented getters and setters and added private Beverage, Starter, MainDish and Dessert attributes
    
    * Revert "On branch Sage"
    
    This reverts commit ff87fb992d2abbac80bb4e6977cb80f2b5161e49.
    
    ---------
    
    Co-authored-by: SageBoots <134312325+SageBoots@users.noreply.github.com>

--- Code/Project/Project/Dish.h
+++ Code/Project/Project/Dish.h
@@ -5,61 +5,67 @@
 #ifndef PROJECT_DISH_H
 #define PROJECT_DISH_H
 
-
 #include "Menu.h"
 #include "Customer.h"
+
 class DishStatus;
+
 class Customer;
+
 class HeadChef;
+
 class Menu;
-class Dish {
-private:
-    Menu* order;
-    DishStatus* prepareState;
-    std::string customerName;
-    int customerTable;
-public:
-    Customer* customer;
-    DishStatus* dishStatus;
-    HeadChef* headChef;
-    Dish* createDish();
-    void dishState();
-    DishStatus* getDishStatus();
-    void change();
-    void setState(DishStatus* state);
-    std::string getCustomerName();
-    void setCustomerName(std::string customerName);
-    int getCustomerTable();
-    void setCustomerTable(int customerTableID);
+
+class Dish
+{
+    private:
+        Menu* order;
+        DishStatus* prepareState;
+        std::string customerName;
+        int customerTable;
+        
+    public:
+        Customer* customer;
+        DishStatus* dishStatus;
+        HeadChef* headChef;
+        Dish* createDish();
+        void dishState();
+        DishStatus* getDishStatus();
+        void change();
+        void setState(DishStatus* state);
+        std::string getCustomerName();
+        void setCustomerName(std::string customerName);
+        int getCustomerTable();
+        void setCustomerTable(int customerTableID);
 };
 
 class DishStatus : public Dish
 {
-public:
-    Dish* dish;
-    virtual void updateDishStatus() = 0;
-    void DishState();
-    std::string getStatus();
-};
+    public:
+        Dish* dish;
+        virtual void updateDishStatus() = 0;
+        void DishState();
+        std::string getStatus();
+    };
+
 class Preparing: public DishStatus
 {
-
-public: void updateDishStatus();
-
-public: void getStatus();
+    public:
+        void updateDishStatus();
+        void getStatus();
 };
+
 class readyForPickUp: public DishStatus
 {
-
-public: void updateDishStatus();
-
-public: void getStatus();
+    public:
+        void updateDishStatus();
+        void getStatus();
 };
+
 class stiiQueued: public DishStatus
 {
-
-public: void updateDishStatus();
-
-public: void getStatus();
+    public:
+        void updateDishStatus();
+        void getStatus();
 };
 #endif //PROJECT_DISH_H
