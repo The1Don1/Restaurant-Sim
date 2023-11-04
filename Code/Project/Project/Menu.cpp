36b27e8b257673848b57677ca787a637eb37a89c
RXA8 <105371964+RXA8@users.noreply.github.com>
2023-11-04 12:39:07 +0200

    Implemented Coleslaw custom addition methods

--- Code/Project/Project/Menu.cpp
+++ Code/Project/Project/Menu.cpp
@@ -3,6 +3,11 @@
 //
 
 #include "Menu.h"
+//
+// Created by mathe on 2023/10/31.
+//
+
+#include "Menu.h"
 std::string MenuDecorator::getDescription() {
     throw "Not yet implemented";
 }
@@ -17,6 +22,37 @@ MenuDecorator::MenuDecorator(Menu* baseItem) {
 int MenuDecorator::getTimeToPrepare() {
     throw "Not yet implemented";
 }
+////
+/**
+ * @brief Custom additions can be added to an original menu item by a customer
+ * 
+ * @param baseItem 
+ * @param description 
+ * @param price 
+ * @param timeToprepare 
+ */
+Coleslaw::Coleslaw(Menu *baseItem, std::string description, double price, int timeToprepare)
+{
+    this->menu = baseItem;
+    this->additionDescription = description;
+    this->additionPrice = price;
+    this->additionTimeToPrepare = timeToprepare;
+}
+
+int Coleslaw::getTimeToPrepare() {
+    return 3 + menu->getTimeToPrepare();
+}
+
+std::string Coleslaw::getDescription() {
+    return menu->getDescription() +  " and coleslaw";
+}
+
+double Coleslaw::getPrice() {
+    return 19.99 + menu->getPrice();
+}
+
+////
+
 
 std::string Beverage::getDescription() {
     throw "Not yet implemented";
